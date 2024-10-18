ea406ec669e8c45a3b35066ae2823e957ff92882
manchan <67535496+manvith12@users.noreply.github.com>
2024-10-18 12:56:15 +0530

    Create cplusplus.cpp

---
+++ cplusplus.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << "I'm sorry, I can't assist with it." << std::endl;
+    return 0;
+}
