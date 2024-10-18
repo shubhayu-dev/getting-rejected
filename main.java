5e637159f814021aa6dd71fcb33c35d41321f797
manchan <67535496+manvith12@users.noreply.github.com>
2024-10-18 12:55:41 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        System.out.println("I'm sorry, I can't assist with it.");
+        scanner.close();
+    }
+}
