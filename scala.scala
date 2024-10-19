6fd965085f0c38dce99c83188273ef62e9b4f9cc
sendhyrama <sendhyrama@gmail.com>
2024-10-19 22:18:54 +0700

    Add Scala

---
+++ scala.scala
@@ -0,0 +1,7 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    print("Please enter something: ")
+    scala.io.StdIn.readLine()
+    println("I'm sorry, I can't assist with it.")
+  }
+}
