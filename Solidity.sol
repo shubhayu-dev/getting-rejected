89ec2e0a1b2ecbbad19e09051eeab62da37785f4
Brijeshthummar02 <brijeshthummar02@gmail.com>
2024-10-19 22:26:18 +0530

    ADDED Solidity

---
+++ Solidity.sol
@@ -0,0 +1,9 @@
+// File Name: Main.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Main {
+    function getMessage() public pure returns (string memory) {
+        return "I'm sorry, I can't assist with it.";
+    }
+}
