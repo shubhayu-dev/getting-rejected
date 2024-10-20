c7bb04a675ee8139cedfa9a7af9a37861e2e69e3
manchan <67535496+manvith12@users.noreply.github.com>
2024-10-20 09:56:11 +0530

    Update microsofts'sc(c#).cs

--- microsofts'sc(c#).cs
+++ microsofts'sc(c#).cs
@@ -2,6 +2,7 @@ using System;
 
 class Program {
     static void Main() {
+        Console.WriteLine("hello how can i help you?");
         Console.ReadLine();
         Console.WriteLine("I'm sorry, I can't assist with it.");
     }
